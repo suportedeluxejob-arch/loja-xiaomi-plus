fe63493d2cd4b2aee5ac1b2b81cda46bc50dced0
suportedeluxejob-arch <suporte.deluxejob@gmail.com>
2026-03-09 17:15:22 -0300

    Fix invalid Google Maps embed URL on contact page

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -98,7 +98,7 @@ export default function ContactPage() {
                                     <span className="text-gray-900 font-mono">Google Maps Embed</span>
                                 </div>
                                 <iframe
-                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1m3!1d3674.341499596001!2d-43.6826!3d-22.9168!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjLCsDU1JzAwLjUiUyA0M8KwNDAnNTcuNCJX!5e0!3m2!1spt-BR!2sbr!4v1610000000000!5m2!1spt-BR!2sbr"
+                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1836.4674751470438!2d-43.6807035!3d-22.9203074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9bfb74782f3543%3A0xd228cef25f40d793!2sXiaomi%20Plus%20-%20VENDA%20DE%20CELULARES%20XIAOMI%2FREALME%2FAPPLE!5e0!3m2!1spt-BR!2sbr!4v1709848450142!5m2!1spt-BR!2sbr"
                                     width="100%"
                                     height="100%"
                                     style={{ border: 0 }}
